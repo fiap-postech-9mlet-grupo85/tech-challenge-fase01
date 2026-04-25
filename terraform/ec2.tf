ba4932c52bb4ef99ef5c9f3a5d14ff47d4248d84
Bruno Abreu <8974710+brunoabreu0@users.noreply.github.com>
2026-04-25 11:02:59 +0100

    feat: implement IAM SSM role for EC2 and add automated deployment workflow via AWS SSM (#22)

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -30,10 +30,37 @@ resource "aws_security_group" "api_sg" {
   }
 }
 
-# 3. Instância EC2 (T2.Micro - Free Tier) que rodará o Docker de forma transparente
+# 3. IAM Role: Conceder permissão para o Github Actions controlar a máquina remotamente via AWS SSM
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name               = "telco_churn_ec2_ssm_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "telco_churn_ec2_ssm_profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+# 4. Instância EC2 (t3.micro - Free Tier) que rodará o Docker de forma transparente
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.amazon_linux_2023.id
-  instance_type = "t3.micro"
+  ami                  = data.aws_ami.amazon_linux_2023.id
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
 
   vpc_security_group_ids = [aws_security_group.api_sg.id]
 
