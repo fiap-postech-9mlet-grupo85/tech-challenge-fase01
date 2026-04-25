ccf7c3dec96ad51ad634bff5cd10df5487165156
Bruno Abreu <8974710+brunoabreu0@users.noreply.github.com>
2026-04-25 11:10:02 +0100

    feat: configure S3 backend for Terraform state storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "fiappostech9mletgrupo32-fase01-tfstate"
+    key    = "terraform.tfstate"
+    region = "sa-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
